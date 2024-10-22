40e730bf3cfb1a2f1ad809b1a59fb003eeb9ef2a
Sambhav Singh <ryugasingh90@gmail.com>
2024-10-22 13:30:02 +0530

    Initial Commit

---
+++ Helmet Detection/Helmet_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helmet_DetectionApp.swift
+//  Helmet Detection
+//
+//  Created by Sambhav Singh on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Helmet_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Helmet DetectionTests/Helmet_DetectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Helmet_DetectionTests.swift
+//  Helmet DetectionTests
+//
+//  Created by Sambhav Singh on 22/10/24.
+//
+
+import Testing
+@testable import Helmet_Detection
+
+struct Helmet_DetectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
